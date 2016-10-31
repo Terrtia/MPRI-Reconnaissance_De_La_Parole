37ef33598b17e784680b3174d01760dab104e6da
Terrtia <or1994@hotmail.fr>
2016-10-31 13:26:24 +0100

    partie2

--- compare.m
+++ compare.m
@@ -12,24 +12,27 @@ vt_size = size(vt,2);
 %calcul du tableau des distance locales
 for i = 1:vb_size
     for j = 1:vt_size
-        d{i,j} = dist(vb{i},vt{j});
+        d{i,j} = dist(vb{i},vt{j}');
     end
 end
 
+
 %calucl du chemin
+%M = distance precedents
 D=d;
 for j =  1:vt_size
     for i = 1:vb_size
-    if j == 1 && i == 1
-        M = 0;
-    elseif i == 1
-        M = D{i,j-1};
-    elseif j == 1
-        M = D{i-1,j};
-    else   
-        M = min(min(D{i,j-1},D{i-1,j-1}),D{i-1,j});
-    end
-    D{i,j}=d{i,j}+ M ;
+        if j == 1 && i == 1
+            M = 0;
+        elseif i == 1
+            M = D{i,j-1};
+        elseif j == 1
+            M = D{i-1,j};
+        else   
+            M = min(min(D{i,j-1},D{i-1,j-1}),D{i-1,j});
+        end
+        
+        D{i,j}=d{i,j}+ M ;
     end
 end
 
