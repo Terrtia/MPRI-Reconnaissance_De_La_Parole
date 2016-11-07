09796ff0869e54cb8fbe6401d70e6b9d3664592f
PetersBrice <brice.peters@laposte.net>
2016-11-07 13:12:25 +0100

    version 1

--- compare.m
+++ compare.m
@@ -12,7 +12,8 @@ vt_size = size(vt,2);
 %calcul du tableau des distance locales
 for i = 1:vb_size
     for j = 1:vt_size
-        d{i,j} = dist(vb{i},vt{j}');
+        %distance euclidienne
+        d{i,j} = sqrt(sum((vb{i}-vt{j}).^2));
     end
 end
 
@@ -20,21 +21,31 @@ end
 %calucl du chemin
 %M = distance precedents
 D=d;
+tab_chemin = d;
 for j =  1:vt_size
     for i = 1:vb_size
         if j == 1 && i == 1
             M = 0;
+            tab_chemin{i,j} = 0;
         elseif i == 1
             M = D{i,j-1};
+            tab_chemin{i,j} = tab_chemin{i,j-1} +1;
         elseif j == 1
             M = D{i-1,j};
+            tab_chemin{i,j} = tab_chemin{i-1,j}+1; 
         else   
             M = min(min(D{i,j-1},D{i-1,j-1}),D{i-1,j});
+            if M == D{i,j-1}
+                tab_chemin{i,j} = 1+tab_chemin{i,j-1};
+            elseif M==D{i-1,j-1}
+                tab_chemin{i,j} = 1+tab_chemin{i-1,j-1};
+            elseif M==D{i-1,j}
+                tab_chemin{i,j} = tab_chemin{i-1,j};
+            end
         end
-        
         D{i,j}=d{i,j}+ M ;
     end
 end
 
-score = D{vb_size,vt_size};
+score = D{vb_size,vt_size}/tab_chemin{vb_size,vt_size};
 end
